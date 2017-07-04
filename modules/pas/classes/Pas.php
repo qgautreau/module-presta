5470972f3d4a77e0029b3a00f36b559401c9f7c5
Quentin Gautreau <qgautreau@gmail.com>
2017-07-04 16:33:40 +0200

    Initial commit

---
+++ modules/pas/classes/Pas.php
@@ -0,0 +1,44 @@
+<?php
+class Pas extends Module
+{
+    public function __construct()
+    {
+        $this->name = 'pas';
+        $this->tab = 'front_office_features';
+        $this->version = '1.0.0';
+        $this->author = 'Pas';
+        $this->need_instance = 0;
+        $this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_);
+        $this->bootstrap = true;
+        parent::__construct();
+        $this->displayName = $this->l('Pas');
+        $this->description = $this->l('Awesome Module made by myself.');
+        $this->confirmUninstall = $this->l('Are you sure you want to uninstall?');
+        if (!Configuration::get('MYMODULE_NAME')) {
+            $this->warning = $this->l('No name provided');
+        }
+    }
+    public function install()
+    {
+        if (Shop::isFeatureActive()) {
+            Shop::setContext(Shop::CONTEXT_ALL);
+        }
+        if (!parent::install()
+            || !$this->registerHook('leftColumn')
+            || !$this->registerHook('header')
+            || !Configuration::updateValue('MYMODULE_NAME', 'my friend')
+        ) {
+            return false;
+        }
+        return true;
+    }
+    public function uninstall()
+    {
+        if (!parent::uninstall()
+            || !Configuration::deleteByName('MYMODULE_NAME')
+        ) {
+            return false;
+        }
+        return true;
+    }
+}
